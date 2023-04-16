76a46e40c181e9092df7eeecfa5da52afc78d02f
Dhruba <93818182+Excaliber917@users.noreply.github.com>
2023-04-17 01:15:20 +0530

    testimonial array has been added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//navbar
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav_list_sm")
 
@@ -7,40 +8,71 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active")
 })
 
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
-const wrapper = document.querySelector(".wrapper");
-const image = wrapper.querySelector(".testi_img");
 
-const imageLinks = [
-    "./assets/1 (3).jpg",
-    "./assets/2.jpg",
-    "./assets/3.jpg",
-    "./assets/1 (4).jpg",
+
+
+//testimonial image
+const testimonials = [
+    {
+        imageSrc: './assets/1 (3).jpg',
+        description: 'one set fifth winged divided seas years, seas is void darkness winged. Season green life Don great said won\'t whales together man lesser is may Itself need moveth herb itself darkness likeness sixth creepeth',
+        name: 'Millard Castinson',
+        designation: 'Software Developer'
+    },
+    {
+        imageSrc: './assets/1 (4).jpg',
+        description: 'two meat and the barbecue sauce complemented each other perfectly, the fries were crispy and well-seasoned, and the coleslaw was creamy and tangy. I would highly recommend this place to anyone who loves barbecue!',
+        name: 'Jane Smith',
+        designation: 'Food Critic'
+    },
+    {
+        imageSrc: './assets/3.jpg',
+        description: 'three meat and the barbecue sauce complemented each other perfectly, the fries were crispy and well-seasoned, and the coleslaw was creamy and tangy. I would highly recommend this place to anyone who loves barbecue!',
+        name: 'lawra Smith',
+        designation: 'Health Critic'
+    },
+    {
+        imageSrc: './assets/2.jpg',
+        description: 'four meat and the barbecue sauce complemented each other perfectly, the fries were crispy and well-seasoned, and the coleslaw was creamy and tangy. I would highly recommend this place to anyone who loves barbecue!',
+        name: 'matt Smith',
+        designation: 'Medical Critic'
+    }
+    // add more testimonials as needed
 ];
 
-let position = 0;
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const image = document.querySelector('.testi_img');
+const description = document.querySelector('.testi_desc_p');
+const name = document.querySelector('.testi_desc_sign');
+const designation = document.querySelector('.testi_designation');
 
-image.src = imageLinks[position];
+let currentIndex = 0;
 
-prevButton.addEventListener("click", () => {
-    if (position > 0) {
-        position--;
-    } else {
-        position = imageLinks.length - 1;
-    }
-    image.src = imageLinks[position];
+function updateTestimonial() {
+    const testimonial = testimonials[currentIndex];
+    image.src = testimonial.imageSrc;
+    description.textContent = testimonial.description;
+    name.textContent = testimonial.name;
+    designation.textContent = testimonial.designation;
+}
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex === 0) ? testimonials.length - 1 : currentIndex - 1;
+    updateTestimonial();
 });
 
-nextButton.addEventListener("click", () => {
-    if (position < imageLinks.length - 1) {
-        position++;
-    } else {
-        position = 0;
-    }
-    image.src = imageLinks[position];
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex === testimonials.length - 1) ? 0 : currentIndex + 1;
+    updateTestimonial();
 });
 
+// Initialize the first testimonial
+updateTestimonial();
+
+
+
+//testimonial video
 const video = document.querySelector("#video");
 const playButton = document.querySelector(".play_button");
 
