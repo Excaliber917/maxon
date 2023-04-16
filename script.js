44a9aed363f785e2c5d7d9a2ae1d621d603c0105
Dhruba <93818182+Excaliber917@users.noreply.github.com>
2023-04-16 19:30:09 +0530

    testimonial image slider is added

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active")
 })
 
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const wrapper = document.querySelector(".wrapper");
+const image = wrapper.querySelector(".testi_img");
+
+const imageLinks = [
+    "./assets/1 (3).jpg",
+    "./assets/2.jpg",
+    "./assets/3.jpg",
+    "./assets/1 (4).jpg",
+];
+
+let position = 0;
+
+image.src = imageLinks[position];
+
+prevButton.addEventListener("click", () => {
+    if (position > 0) {
+        position--;
+    } else {
+        position = imageLinks.length - 1;
+    }
+    image.src = imageLinks[position];
+});
+
+nextButton.addEventListener("click", () => {
+    if (position < imageLinks.length - 1) {
+        position++;
+    } else {
+        position = 0;
+    }
+    image.src = imageLinks[position];
+});
